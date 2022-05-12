7f828621379b5d5200a893df192e25eb3dd8f1af
Shashank HJ <pratheek.xworkz@gmail.com>
2022-05-12 13:45:59 +0530

    test

---
+++ Eclipse Program/TestThursday/src/com/xworkz/test/Test.java
@@ -0,0 +1,36 @@
+package com.xworkz.test;
+
+public class Test {
+		public static void main(String[] args) {
+
+			/*
+			 * //6)JVM
+			 *  //8)Error and Exception class 
+			 *  //9)Heap Memory 
+			 *  //11)50; 
+			 *  //12)129;
+			 * //13) Autobxing is a process where Primitive data Types are converted into
+			 * Non Primitive data type for example int is converted into Integer ,float is
+			 * Converted into Float; Unboxing is a process where the non promitive data
+			 * types are converetd intopromitive data types; these are implicit conversions
+			 * which are done by the syatem. The main use of Converting a Primitive data
+			 * type to non primitive is it can use inbuilt methods where as Primitives
+			 * cannot use inbuilt methods; 
+			 * 15)Wrapper Classes are used to convert Primitive
+			 * data types into Non Primitive data types explicitly a)using constructor
+			 * b)Autoboxing;
+			 */
+
+			// 18)A String Can be can be convereted into an Integer using getInteger
+            //14th Wrapping and Unwrapping
+			int num =9;
+             Integer num1=Integer.valueOf(num);
+             System.out.println(num1);
+             
+             Integer newNum=10;
+             int numa=newNum.intValue();
+             System.out.println(numa);
+             Integer.ge
+		}
+	}
+
